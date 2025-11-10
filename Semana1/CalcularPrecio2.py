d0fa2ad822a8c5597c8b60d9f663e4cb071e62db
Duvan Roman <duvan.roman07@gmail.com>
2025-11-10 07:36:24 -0500

    Semana 1 con el taller de moodle

--- Semana1/CalcularPrecio2.py
+++ Semana1/CalcularPrecio2.py
@@ -1,32 +1,40 @@
 print("Bienvenido a la calculadora de precio")
 print("*" * 40)
 
+entrada = input("Quieres ingresar datos (si / no): ")
 
-while True:
-    NombreDelProducto = input("Ingresa el nombre del producto: ").strip()
-    if NombreDelProducto.isalpha() or " " in NombreDelProducto:
-        break
-    else:
-        print("Dato erróneo... el nombre debe contener solo letras.\n")
+while entrada.lower() == "si":
+    while True:
+        NombreDelProducto = input("Ingresa el nombre del producto: ")
+        if NombreDelProducto.isnumeric() or NombreDelProducto == "":
+            print("Dato erróneo... el nombre debe contener solo letras.\n")
+            
+        else:
+            break
 
 
-while True:
-    precio= input("Ingresa el precio de el producto: ")
-    try:
-        precio = float(precio)
-        break
-    except ValueError:
-        print("Datos erroneos, solo se pueden ingresar numeros.. Vuelve a intentarlo\n")
-        
+    while True:
+        precio= input("Ingresa el precio de el producto: ")
+        try:
+            precio = float(precio)
+            break
+        except ValueError:
+            print("Datos erroneos, solo se pueden ingresar numeros.. Vuelve a intentarlo\n")
+            
 
-while True:
-    cantidad= input("Ingresa la cantidad de el producto: ")
-    try:
-        cantidad = int(cantidad)
-        break
-    except ValueError:
-        print("Datos erroneos, solo se pueden ingresar numeros... vuelve a intentarlo\n")
+    while True:
+        cantidad= input("Ingresa la cantidad de el producto: ")
+        try:
+            cantidad = int(cantidad)
+            break
+        except ValueError:
+            print("Datos erroneos, solo se pueden ingresar numeros... vuelve a intentarlo\n")
+    costoTotal= precio * cantidad
 
-costoTotal= precio * cantidad
+    print(f"\nProducto: {NombreDelProducto} | Precio: {precio:.3f} | Cantidad:{cantidad} | Total: {costoTotal:.3f} ")
+    entrada = input("Quieres ingresar datos (si / no): ")
 
-print(f"\nProducto: {NombreDelProducto} | Precio: {precio} | Cantidad:{cantidad} | Total: {costoTotal} ")
+print("Gracias por su compra, vuelva pronto\n")  
+
+
+
