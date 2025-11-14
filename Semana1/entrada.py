8c689efb0fc62903cea5023611ca38463ffa9770
Duvan Roman <duvan.roman07@gmail.com>
2025-11-14 13:39:32 -0500

    Semana2, day 5

--- Semana1/entrada.py
+++ Semana1/entrada.py
@@ -6,6 +6,10 @@ boleto= input("\nTienes el boleto: (si o no)...")
 
 if edad >= 18 and boleto.lower() == "si":
     print("\n ¡¡Puedes ingresar!!")
+elif edad >= 18 and boleto.lower() == "no":
+    print("\n Compre el boleto o ¿Está desempleado?\n ")
+elif edad < 18 and boleto.lower() == "no":
+    print("\n No se ni pa que viniste pendejo!!\n ")
 else:
     print("\n Eres menor de edad Devuelva el boleto pa revenderlo y .. \n ¡¡Pa la casa gonorrea!!\n")
         
